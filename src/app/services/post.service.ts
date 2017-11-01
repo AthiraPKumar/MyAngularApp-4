afe6c0d06bc52fd50364878cada227ec3ee4af7d
AthiraPKumar <athirapkumar16@gmail.com>
2017-11-01 16:17:44 +0530

    refactored and added the base class called dataservice and placed the api url here

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,43 +1,12 @@
+import { DataService } from './data.service';
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { NotFoundError } from './../common/not-found-error';
-import { BadInput } from './../common/bad-input';
-import { AppError } from './../common/app-error';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
+
 
 @Injectable()
-export class PostService {
-private url= 'http://jsonplaceholder.typicode.com/posts';
+export class PostService extends DataService {
 
-  constructor(private http: Http) {}
-
-  getPosts(){
-  	return this.http.get(this.url);
-  }
-
-  createPost(post){
-  	return this.http.post(this.url,JSON.stringify(post))
-    .catch((error: Response) => {
-      if(error.status === 400)
-        return Observable.throw(new BadInput(error.json()));
-
-      return Observable.throw(new AppError(error.json()));
-    });
-  }
-
-  updatePost(post){
-  	return this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead:true }));
-  }
-
-  deletePost(id){
-  	return this.http.delete(this.url + '/' + id)
-    .catch((error : Response) => {
-      if(error.status === 404)
-        return Observable.throw(new NotFoundError());
-      return Observable.throw(new AppError(error));
-    });
-  }
-
+  constructor(http: Http) {
+    super('http://jsonplaceholder.typicode.com/posts', http);
+  }   
 }

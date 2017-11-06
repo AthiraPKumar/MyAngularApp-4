47699f561e472ce56437c6014a65a9ef693fe188
AthiraPKumar <athirapkumar16@gmail.com>
2017-11-06 11:17:34 +0530

    for postservice api url changed from http to https,since error ws throwing.

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class PostService extends DataService {
 
   constructor(http: Http) {
-    super('http://jsonplaceholder.typicode.com/posts', http);
+    super('https://jsonplaceholder.typicode.com/posts', http);
   }   
 }
 

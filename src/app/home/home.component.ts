ec5ad940db704a63239f0e12f875560d9a9b1330
AthiraPKumar <athirapkumar16@gmail.com>
2017-12-22 22:58:38 +0530

    archive json added in the component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
+	archives = [
+		{ year:2017, month:1 },
+		{ year:2018, month:2 },
+		{ year:2019, month:3 }
+	];
   constructor() { }
 
   ngOnInit() {

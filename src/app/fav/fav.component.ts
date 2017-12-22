d99ece987e197d556ac0c6fe7f4431c3605ceef1
AthiraPKumar <athirapkumar16@gmail.com>
2017-12-22 22:51:01 +0530

    test-fav component

---
+++ src/app/fav/fav.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fav',
+  templateUrl: './fav.component.html',
+  styleUrls: ['./fav.component.css']
+})
+export class FavComponent {
+	isFav: boolean;
+
+  constructor() { }
+
+  onClick(){
+  	this.isFav = !this.isFav;
+  	console.log(this.isFav);
+  }
+
+
+}

a724e1066d742c418c3defd13fccc3d1f0d5b539
AthiraPKumar <athirapkumar16@gmail.com>
2017-12-26 00:18:45 +0530

    json created of consumer, methods created

--- src/app/detail-view/detail-view.component.ts
+++ src/app/detail-view/detail-view.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class DetailViewComponent {
 	viewMode = 'map';
+
+	consumers = [
+		{id:1 , name:"Athira", status:"Active"},
+		{id:2 , name:"Maya", status:"Active"},
+		{id:3 , name:"Parul", status:"Active"},
+	];
+
+	onAdd(){
+		this.consumers.push({id:4, name:"Clint", status:"Active" });
+	};
+	onRemove(people){
+		let index = this.consumers.indexOf(people);
+		this.consumers.splice(index, 1);
+	};
+	onChange(people){
+		people.status ="Inactive";
+	};
 }
